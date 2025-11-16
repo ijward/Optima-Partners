485715e27442f57d58ffee81e6f9cc89318b52e9
ijward <ianjward1978@sky.com>
2025-11-16 19:33:01 +0000

    updates

--- solution/main.py
+++ solution/main.py
@@ -1,7 +1,8 @@
 import pandas as pd
-import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+os.chdir(os.path.dirname(os.getcwd()))
+
+cwd=os.getcwd()
 from functions.csv_to_pandas import import_csv_to_df
 
 cwd=os.getcwd()
